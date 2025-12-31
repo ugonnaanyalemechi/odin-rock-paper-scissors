a4fe15d8d2fce0bc4eddb3b456c9dee713035d02
ugonna <anyugonna@gmail.com>
2025-12-30 18:26:12 -0600

    Add getHumanChoice function

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function getComputerChoice() {
   return Math.floor(Math.random() * 3);
 }
  
+function getHumanChoice() {
+  let choice = prompt("Choose: Rock, paper, or scissors?");
+  return choice;
+}
+
