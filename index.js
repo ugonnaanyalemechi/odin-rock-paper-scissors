5c549b7452742def8272d412dc375359045e6ebc
ugonna <anyugonna@gmail.com>
2026-01-04 16:03:14 -0600

    Complete basic game functionality with bug where game does not end until someone reaches 5 points

--- index.js
+++ index.js
@@ -15,44 +15,50 @@ function getHumanChoice() {
   return choice;
 }
 
+// changed this from returning winner based on numbers to winner based on string
 function playRound(humanChoice, computerChoice) {
   humanChoice = humanChoice.toLowerCase();
 
-  let winner = 0; // 0 represents human, 1 represents computer, 2 represents draw
+  let roundWinner = "";
   
-  const roundResults = document.querySelector(".round-results");
+  const roundResults = document.querySelector(".game-results");
   const roundResult = document.createElement("p");
 
   if (humanChoice === "rock" && computerChoice === "scissors") {
     roundResult.textContent = "You win! Rock beats scissors!";
+    roundWinner = "human";
   } else if (humanChoice === "scissors" && computerChoice === "rock") {
     roundResult.textContent = "You lose! Rock beats scissors!";
-    winner = 1;
+    roundWinner = "computer";
   } else if (humanChoice === "scissors" && computerChoice === "paper") {
     roundResult.textContent = "You win! Scissors beats paper!";
+    roundWinner = "human";
   }  else if (humanChoice === "paper" && computerChoice === "scissors") {
     roundResult.textContent = "You lose! Scissors beats paper!";
-    winner = 1;
+    roundWinner = "computer";
   } else if (humanChoice === "paper" && computerChoice === "rock") {
     roundResult.textContent = "You win! Paper beats rock!";
+    roundWinner = "human";
   } else if (humanChoice === "rock" && computerChoice === "paper") {
     roundResult.textContent = "You lose! Paper beats rock!";
-    winner = 1;
+    roundWinner = "computer";
   } else {
     roundResult.textContent = "Draw!";
-    winner = 2;
   }
 
   roundResults.appendChild(roundResult);
+  return roundWinner;
 }
 
-function determineGameWinner(humanScore, computerScore) {
-  if (humanScore > computerScore) {
-    console.log("You win the game!");
-  } else if (computerScore > humanScore) {
-    console.log("You lose the game!");
-  } else {
-    console.log("The game is a draw!");
+function checkGameWinner(humanScore, computerScore) {
+  if (humanScore === 5) {
+    const roundResults = document.querySelector(".game-results");
+    const winnerResult = document.createElement("p");
+    winnerResult.textContent = "You win the game!";
+  } else if (computerScore === 5) {
+    const roundResults = document.querySelector(".game-results");
+    const winnerResult = document.createElement("p");
+    winnerResult.textContent = "You lose the game!";
   }
 }
 
@@ -60,17 +66,45 @@ function playGame() {
   let humanScore = 0;
   let computerScore = 0;  
 
-  determineGameWinner(humanScore, computerScore);
+  const selectionButtons = document.querySelector(".buttons");
+
+  selectionButtons.addEventListener("click", (e) => {
+    const computerSelection = getComputerChoice();
+    const humanSelection = e.target.classList[0];
+    const roundWinner = playRound(humanSelection, computerSelection);
+    
+    const humanScorePara = document.querySelector(".human-score");
+    const computerScorePara = document.querySelector(".computer-score");
+
+    if (roundWinner === "human") {
+      humanScore += 1;
+      humanScorePara.textContent = `Human Score: ${humanScore}`;
+
+    } else if (roundWinner === "computer") {
+      computerScore += 1;
+      computerScorePara.textContent = `Computer's Score: ${computerScore}`;
+    }
+
+    if (humanScore === 5) {
+      const gameResults = document.querySelector(".game-results");
+      const winnerResult = document.createElement("p");
+      winnerResult.textContent = "You win the game!";
+      gameResults.append(winnerResult);
+    } else if (computerScore === 5) {
+      const gameResults = document.querySelector(".game-results");
+      const winnerResult = document.createElement("p");
+      winnerResult.textContent = "You lose the game!";
+      gameResults.append(winnerResult);
+    }
+  });
+
+  if (humanScore === 5 || computerScore === 5) {
+    return;
+  }
 }
 
-const selectionButtons = document.querySelector(".buttons");
+playGame();
 
-selectionButtons.addEventListener("click", (e) => {
-  const computerSelection = getComputerChoice();
-  const humanSelection = e.target.classList[0];
-  playRound(humanSelection, computerSelection);
-});
-
-
+// create initialize game function? (storing )
 
 
